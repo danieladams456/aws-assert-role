7806c27aa55f44b06d12bc665ccb7fae8cbc68a8
Daniel Adams <daniel.adams@genworth.com>
2021-01-14 19:32:06 -0500

    replicate low level signature, now try higher level via middleware capturing request

---
+++ Readme.md
@@ -0,0 +1,3 @@
+[packages/signature-v4/src/SignatureV4.ts](https://github.com/aws/aws-sdk-js-v3/blob/master/packages/signature-v4/src/SignatureV4.ts) is what does the signing
+
+gets called in the middleware [here](https://github.com/aws/aws-sdk-js-v3/blob/master/packages/middleware-signing/src/middleware.ts)
---
+++ v4.js
@@ -0,0 +1,40 @@
+import { HttpRequest } from "@aws-sdk/protocol-http";
+import { Sha256 } from "@aws-crypto/sha256-js";
+const { SignatureV4 } = require("@aws-sdk/signature-v4");
+
+// const minimalRequest = new HttpRequest({
+//   method: "POST",
+//   protocol: "https:",
+//   path: "/",
+//   headers: {
+//     host: "foo.us-bar-1.amazonaws.com",
+//   },
+//   hostname: "foo.us-bar-1.amazonaws.com",
+// });
+const minimalRequest = new HttpRequest({
+  method: "POST",
+  protocol: "https:",
+  path: "/",
+  headers: {
+    host: "foo.us-bar-1.amazonaws.com",
+  },
+  hostname: "foo.us-bar-1.amazonaws.com",
+});
+
+const credentials = {
+  accessKeyId: "foo",
+  secretAccessKey: "bar",
+};
+const signerInit = {
+  service: "foo",
+  region: "us-bar-1",
+  sha256: Sha256,
+  credentials,
+};
+
+const signer = new SignatureV4(signerInit);
+const signed = await signer.presign(minimalRequest, {
+  signingDate: new Date("2000-01-01T00:00:00.000Z"),
+});
+
+console.log(signed);
