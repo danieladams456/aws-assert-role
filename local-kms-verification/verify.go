8c4d5d2b57f75231ecab9daf22e0993741bff1fb
Daniel Adams <daniel.adams@genworth.com>
2021-01-18 17:32:05 -0500

    verify jwt test

--- local-kms-verification/verify.go
+++ local-kms-verification/verify.go
@@ -5,14 +5,19 @@ import (
 	"crypto/rsa"
 	"crypto/sha512"
 	"encoding/base64"
+	"errors"
 	"fmt"
 )
 
 func verifySignature(message, signature string, pubKey *rsa.PublicKey) error {
 	sigBytes, err := base64.StdEncoding.DecodeString(signature)
 	if err != nil {
-		return fmt.Errorf("Error decoding signature: %s\n", err)
+		return fmt.Errorf("error decoding signature: %s", err)
 	}
 	hashed := sha512.Sum512([]byte(message))
 	return rsa.VerifyPKCS1v15(pubKey, crypto.SHA512, hashed[:], sigBytes)
+}
+
+func verifyJWT(jwt string, pubKey *rsa.PublicKey) error {
+	return errors.New("not implemented")
 }
